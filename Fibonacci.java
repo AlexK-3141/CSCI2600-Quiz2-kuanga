ceced61ac0892e0aebc1397cc2f1b94eae82e4f4
AlexK-3141 <125620111+AlexK-3141@users.noreply.github.com>
2025-05-29 14:13:41 -0400

    added Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,28 @@
+class Fibonacci {
+
+	public long fibHelper(long n, long a, long b) {
+		if (n == 0) {
+			return a;
+		} else if (n == 1) {
+			return b;
+		}
+		return fibHelper(n - 1, b, a + b);
+	}
+	
+	public long getFibTerm(long n) {
+		if (n < 0) {
+			throw new IllegalArgumentException(n + " is negative");
+		} else {
+			return fibHelper(n, 0, 1);
+		}	        
+	}
+	
+	public static void main(String[] argv) {
+		Fibonacci fib = new Fibonacci();
+		for (int i = 0; i <= 20; i += 1) {
+			long fibTerm = fib.getFibTerm(i);
+			System.out.println(i + "th Fibonacci term: " + fibTerm);
+		}
+	}
+	
+}
